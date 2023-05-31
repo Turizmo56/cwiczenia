a116ad58494a12d2422dfe4db13b1bdc39b6eaea
Turizmo56 <e.schmitdke123@gmail.com>
2023-05-31 13:23:10 +0200

    dodany palindron dodany anagram

--- skrypt2/main.py
+++ skrypt2/main.py
@@ -22,3 +22,21 @@ def palindron(napis):
         return True
     if __name == "__palindron__":
         palindron(napis)
+
+
+def anagram(napis1, napis2):
+    n = len(napis1)
+    if n != len(napis2):
+        return False
+    slownik1 = dict()
+    slownik2 = dict()
+    for i in range (n):
+        if napis1[i] in slownik1:
+            slownik1[napis1[i]] += 1
+        else:
+            slownik1[napis1[i]] = 1
+        if napis2[i] in slownik2:
+            slownik2[napis2[i]] += 1
+        else:
+            slownik2[napis2[i]] = 1
+            return slownik1 == slownik2

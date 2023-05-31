66f508c8d669bf3ddd5adcd2eb69a38f52fd912f
Turizmo56 <e.schmitdke123@gmail.com>
2023-05-31 14:26:41 +0200

    dodany palindron dodany anagram naprawiony anagram oraz dodana funkcja main ktora wszystkie skrypty wywoluje

--- skrypt2/main.py
+++ skrypt2/main.py
@@ -10,33 +10,35 @@ def potegi(podstawa, wykladnik):
         return polowa * polowa * podstawa
 
 
-if __name__ == "__potegi__":
-    potegi(podstawa, wykladnik)
-
-
 def palindron(napis):
-    n = len(napis)
-    for i in range(len(napis) // 2):
-        if napis[i] != napis[n - 1 - 1]:
+    return napis == napis[::-1]
+
+
+def anagram(napis1,napis2):
+        n = len(napis1)
+        if n != len(napis2):
             return False
-        return True
-    if __name == "__palindron__":
-        palindron(napis)
+        slownik1 = dict()
+        slownik2 = dict()
+        for i in range(n):
+            if napis1[i] in slownik1:
+                slownik1[napis1[i]] += 1
+            else:
+                slownik1[napis1[i]] = 1
+            if napis2[i] in slownik2:
+                slownik2[napis2[i]] += 1
+            else:
+                slownik2[napis2[i]] = 1
+        return slownik1 == slownik2
 
 
-def anagram(napis1, napis2):
-    n = len(napis1)
-    if n != len(napis2):
-        return False
-    slownik1 = dict()
-    slownik2 = dict()
-    for i in range (n):
-        if napis1[i] in slownik1:
-            slownik1[napis1[i]] += 1
-        else:
-            slownik1[napis1[i]] = 1
-        if napis2[i] in slownik2:
-            slownik2[napis2[i]] += 1
-        else:
-            slownik2[napis2[i]] = 1
-            return slownik1 == slownik2
+def main():
+    print("2^5 = " + str(potegi(2, 5)))
+    print("CzyPalindrom(kajak) = " + str(palindron("kajak")))
+    print("CzyPalindrom(kobyla) = " + str(palindron("kobyla")))
+    print("Czy anagram(kwiat, tkwia) = " + str(anagram("kwiat","tkwia")))
+    print("czy anagram(kobyla,boczek) = " + str(anagram("kobyla", "boczek")))
+
+
+if __name__ == "__main__":
+    main()
